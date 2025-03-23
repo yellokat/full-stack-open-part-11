ccad77674a9afb4f12226f2bd5dc6f43ef4c183b
Yellokat <jenova195@gmail.com>
2025-03-23 23:45:29 +0200

    test : intentionally break /health endpoint

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('3')
+  res.send('4')
 })
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
 
