f4dd772143e794499a6c23b49f1a2c2e654dc9c8
Yellokat <jenova195@gmail.com>
2025-03-23 23:46:15 +0200

    fix : restore /health endpoint

--- app.js
+++ app.js
@@ -8,12 +8,10 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('4')
+  res.send('5')
 })
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
   res.send('ok')
 })
 
