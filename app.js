aac691d963fbbc7a4faedc0559cb809104b8fc7c
Yellokat <jenova195@gmail.com>
2025-03-24 03:53:08 +0200

    feat : part 11 - exercise 16

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('6')
+  res.send('7')
 })
 
 app.get('/health', (req, res) => {
